c1d805cce4c034e72e7d621f4a4f6da857c89e2c
Nithin S Varrier <nithin486@hotmail.com>
2024-12-03 07:58:33 +0000

    fix: fmt

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -10,7 +10,9 @@ pub fn part_one(input: &str) -> Option<i32> {
     let lines = input.lines();
     let mut sum = 0i32;
     for line in lines {
-        for (_, [left_num_str, right_num_str]) in RE_PART_ONE.captures_iter(line).map(|c| c.extract()) {
+        for (_, [left_num_str, right_num_str]) in
+            RE_PART_ONE.captures_iter(line).map(|c| c.extract())
+        {
             let left_num = left_num_str.parse::<i32>().unwrap_or_default();
             let right_num = right_num_str.parse::<i32>().unwrap_or_default();
             sum += right_num * left_num
@@ -25,7 +27,9 @@ pub fn part_two(input: &str) -> Option<i32> {
     let mut sum = 0i32;
     let mut dont = false;
     for line in lines {
-        for (_, [left_num_str, right_num_str]) in RE_PART_TWO.captures_iter(line).map(|c| c.extract()) {
+        for (_, [left_num_str, right_num_str]) in
+            RE_PART_TWO.captures_iter(line).map(|c| c.extract())
+        {
             match (left_num_str, right_num_str) {
                 ("do", "n't") => dont = true,
                 ("d", "o") => dont = false,
