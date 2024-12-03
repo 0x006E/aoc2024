e47bd70f15cda37d2c47f7215e4482edf6d66847
Nithin S Varrier <nithin486@hotmail.com>
2024-12-03 07:51:10 +0000

    fix: change regex to lazy static

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,12 +1,16 @@
 advent_of_code::solution!(3);
 use regex::Regex;
 
+lazy_static::lazy_static! {
+    static ref RE_PART_ONE: Regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    static ref RE_PART_TWO: Regex = Regex::new(r"mul\((\d+),(\d+)\)|(d)(o)\(\)|(do)(n't)\(\)").unwrap();
+}
+
 pub fn part_one(input: &str) -> Option<i32> {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let lines = input.lines();
     let mut sum = 0i32;
     for line in lines {
-        for (_, [left_num_str, right_num_str]) in re.captures_iter(line).map(|c| c.extract()) {
+        for (_, [left_num_str, right_num_str]) in RE_PART_ONE.captures_iter(line).map(|c| c.extract()) {
             let left_num = left_num_str.parse::<i32>().unwrap_or_default();
             let right_num = right_num_str.parse::<i32>().unwrap_or_default();
             sum += right_num * left_num
@@ -17,12 +21,11 @@ pub fn part_one(input: &str) -> Option<i32> {
 }
 
 pub fn part_two(input: &str) -> Option<i32> {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)|(d)(o)\(\)|(do)(n't)\(\)").unwrap();
     let lines = input.lines();
     let mut sum = 0i32;
     let mut dont = false;
     for line in lines {
-        for (_, [left_num_str, right_num_str]) in re.captures_iter(line).map(|c| c.extract()) {
+        for (_, [left_num_str, right_num_str]) in RE_PART_TWO.captures_iter(line).map(|c| c.extract()) {
             match (left_num_str, right_num_str) {
                 ("do", "n't") => dont = true,
                 ("d", "o") => dont = false,
