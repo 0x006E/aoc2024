4412549523eb9f156b23232446a0e8e365bf0b9b
Nithin S Varrier <nithin486@hotmail.com>
2024-12-17 16:20:46 +0000

    fix: xlippy

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,5 +1,5 @@
 use core::panic;
-use std::{fmt::Debug, u64};
+use std::fmt::Debug;
 
 use itertools::Itertools;
 use num_traits::pow;
@@ -8,14 +8,14 @@ advent_of_code::solution!(17);
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 enum Instructions {
-    ADV(u8),
-    BXL(u8),
-    BST(u8),
-    JNZ(usize),
-    BXC(u8),
-    OUT(u8),
-    BDV(u8),
-    CDV(u8),
+    Adv(u8),
+    Bxl(u8),
+    Bst(u8),
+    Jnz(usize),
+    Bxc(u8),
+    Out(u8),
+    Bdv(u8),
+    Cdv(u8),
 }
 
 fn parse_digits_from_string(input: &str) -> u64 {
@@ -38,14 +38,14 @@ fn parse_instructions(input: &str) -> (Vec<u64>, Vec<Instructions>) {
             input_string.push(opcode.parse::<u64>().unwrap());
             input_string.push(operand_num as u64);
             match opcode {
-                "0" => Instructions::ADV(operand_num),
-                "1" => Instructions::BXL(operand_num),
-                "2" => Instructions::BST(operand_num),
-                "3" => Instructions::JNZ(operand_num as usize),
-                "4" => Instructions::BXC(operand_num),
-                "5" => Instructions::OUT(operand_num),
-                "6" => Instructions::BDV(operand_num),
-                "7" => Instructions::CDV(operand_num),
+                "0" => Instructions::Adv(operand_num),
+                "1" => Instructions::Bxl(operand_num),
+                "2" => Instructions::Bst(operand_num),
+                "3" => Instructions::Jnz(operand_num as usize),
+                "4" => Instructions::Bxc(operand_num),
+                "5" => Instructions::Out(operand_num),
+                "6" => Instructions::Bdv(operand_num),
+                "7" => Instructions::Cdv(operand_num),
                 _ => panic!("Invalid instructions!!: {},{}", opcode, operand_num),
             }
         })
@@ -58,7 +58,7 @@ pub fn part_one(input: &str) -> Option<String> {
     let reg_a = parse_digits_from_string(lines.next().unwrap());
 
     let (_, instructions) = parse_instructions(lines.nth(3)?);
-    Some(run(&instructions, reg_a as u64).iter().join(","))
+    Some(run(&instructions, reg_a).iter().join(","))
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -100,7 +100,7 @@ pub fn part_two(input: &str) -> Option<u64> {
     Some(result)
 }
 
-fn run(ins: &Vec<Instructions>, reg_a_orig: u64) -> Vec<u64> {
+fn run(ins: &[Instructions], reg_a_orig: u64) -> Vec<u64> {
     let mut reg_a = reg_a_orig;
     let mut reg_b = 0u64;
     let mut reg_c = 0u64;
@@ -115,36 +115,36 @@ fn run(ins: &Vec<Instructions>, reg_a_orig: u64) -> Vec<u64> {
             6 => reg_c,
             _ => panic!("Invalid combo operand, {}", op),
         }
-    };
+    }
 
     while i < ins.len() {
         let current_ins = &ins[i];
         i += 1;
         match current_ins {
-            Instructions::ADV(o) => {
+            Instructions::Adv(o) => {
                 reg_a /= pow(2, map_combo_operand(*o, reg_a, reg_b, reg_c) as usize) as u64
             }
-            Instructions::BXL(o) => {
+            Instructions::Bxl(o) => {
                 reg_b ^= *o as u64;
             }
-            Instructions::BST(o) => {
+            Instructions::Bst(o) => {
                 reg_b = map_combo_operand(*o, reg_a, reg_b, reg_c) % 8;
             }
-            Instructions::JNZ(o) => {
+            Instructions::Jnz(o) => {
                 if reg_a != 0 {
                     i = *o;
                 }
             }
-            Instructions::BXC(_) => {
+            Instructions::Bxc(_) => {
                 reg_b ^= reg_c;
             }
-            Instructions::OUT(o) => {
+            Instructions::Out(o) => {
                 out.push(map_combo_operand(*o, reg_a, reg_b, reg_c) % 8);
             }
-            Instructions::BDV(o) => {
+            Instructions::Bdv(o) => {
                 reg_b = reg_a / pow(2, map_combo_operand(*o, reg_a, reg_b, reg_c) as usize) as u64
             }
-            Instructions::CDV(o) => {
+            Instructions::Cdv(o) => {
                 reg_c = reg_a / pow(2, map_combo_operand(*o, reg_a, reg_b, reg_c) as usize) as u64
             }
         }
